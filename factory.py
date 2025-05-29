7950661da4be54dc1fb7e0c4c8f892c41c0994aa
GordeevK <Gordeevka05@mail.ru>
2025-05-30 00:34:28 +0500

    a

--- factory.py
+++ factory.py
@@ -6,34 +6,42 @@ class Animal(ABC):
     def make_sound(self):
         pass
 
+
 class Lion(Animal):
     def make_sound(self):
         return "Рычание!"
+
 
 class Monkey(Animal):
     def make_sound(self):
         return "Визг!"
 
+
 class Elephant(Animal):
     def make_sound(self):
         return "Трубление!"
+
 
 class AnimalFactory(ABC):
     @abstractmethod
     def create_animal(self) -> Animal:
         pass
 
+
 class LionFactory(AnimalFactory):
     def create_animal(self) -> Animal:
         return Lion()
+
 
 class MonkeyFactory(AnimalFactory):
     def create_animal(self) -> Animal:
         return Monkey()
 
+
 class ElephantFactory(AnimalFactory):
     def create_animal(self) -> Animal:
         return Elephant()
+
 
 def interact_with_animal(factory: AnimalFactory):
     animal = factory.create_animal()
